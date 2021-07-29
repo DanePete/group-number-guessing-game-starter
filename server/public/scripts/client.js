3efef84505b3c88a437eb2037ea87de4669e8a01
Dane Petersen <dane.m.petersen@gmail.com>
2021-07-29 12:33:15 -0500

    added input and buttons to index and started fcalling of functions on button click

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,5 +1,21 @@
-$(document).ready(handleReady);
+$(document).ready(onReady);
 
-function handleReady() {
-  console.log("jquery is loaded!")
+function onReady() {
+  console.log("jquery is loaded!");
+  $(document).on(`click`,`#submit`, submitGuess);
+  $(document).on(`click`,`#submit`, roundCounter);
+
+
 }
+
+//Grab input from .html input fields
+function submitGuess(){
+
+}//end submitGuess
+
+// Determine submission count 
+let count = 0;
+function roundCounter(){
+  count++;
+   
+}//end roundCounter
